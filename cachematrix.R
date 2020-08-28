ba49be9972d974ce6e90c56849b1d1c59be180dc
Ajderzee <ajder.official@gmail.com>
2020-08-29 02:29:56 +0530

    Both functions (makeCacheMatrix $ cacheSolve) has been added to the R script.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+#below is the function to create special matrix object
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x,r){
+  
+  #x is the matrix that you want to create
+  #r is the number of rows which is also equal to number of columns (square matrix)
+  
+  mat<-matrix(x,nrow=r,ncol=r)
+  inverse<-NULL
+  
+  fset<-function(y,r){      #function to edit a created matrix
+    
+    mat<<-matrix(y,nrow=r,ncol=r)
+    inverse<<-NULL
+    
+  }
+  
+  fget<-function() {        #function to get the matrix
+    
+    return (mat)
+    
+  }
+  
+  fgetinverse<-function(){    #function to get the inverse of the matrix,if stored in cache
+    
+    return (inverse)
+    
+  }
+  
+  fsetinverse<-function(i){     #function to store the inverse in cache
+    
+    inverse<<-i
+    
+  }
+  
+  list(setmatrix=fset,getmatrix=fget,getinverse=fgetinverse,setinverse=fsetinverse)
+  
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#below is the function to get the inverse
+
+cacheSolve<-function(x){
+  
+  #x is the passed object created using function makeCacheMatrix
+  
+  inverse<-x$getinverse()
+  
+  if(!is.null(inverse)){
+    
+    print("Inverse already calculated and cached")
+    return(inverse)
+  }
+  
+  
+  inverse<-solve(x$getmatrix())
+  x$setinverse(inverse)
+  
+  print("Inverse calculated now and stored in cache for future use")
+  x$getinverse()
+  
+  
 }
